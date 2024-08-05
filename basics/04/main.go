7eed357a7664ee720e80f5460af6ce9cb2d31bed
Nisarg2061 <khodkenisarg@gmail.com>
2024-08-06 01:42:04 +0530

    Added about pointers, structs and methods

--- basics/04/main.go
+++ basics/04/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main()  {
-  
+  varr := 2
+  ptr := &varr
+
+  fmt.Println(*ptr)
+  fmt.Println(&varr)
+  fmt.Println(ptr)
+  fmt.Println(&ptr)
 }
---
+++ basics/05/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+  U1 := User{"Nisarg", "n@gmail.com", 1, true}
+  U1.GetStatus()
+}
+
+type User struct {
+  Name string
+  Email string
+  Number int
+  Status bool
+}
+
+func (u User) GetStatus() {
+  setStatus := "Offline"
+  if u.Status == true {
+    setStatus = "Online"
+  }
+  fmt.Printf("Staus of user %v is %v\n", u.Name, setStatus)
+}
