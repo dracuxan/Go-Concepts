d6683f3731f537f63f17a4404a07d57d75214457
dracuxan <khodkenisarg@gmail.com>
2025-04-23 00:02:31 +0530

    new
    add new concepts dir

---
+++ main/hello_world.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func helloWorld(name string) {
+	fmt.Printf("Hello %s!\n", name)
+}
---
+++ main/new_world.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func oldWorld() {
+	fmt.Printf("Welcome to a new world!\n")
+}
