bbcabba8659e63acee493946f9ae399089528360
dracuxan <khodkenisarg@gmail.com>
2025-04-24 18:00:56 +0530

    add
    add functions and test cases for finding sum of integers in a slice and
    tails

--- tdd/arrays/sum.go
+++ tdd/arrays/sum.go
@@ -6,3 +6,25 @@ func Sum(nums []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numsToSum ...[]int) []int {
+	var sums []int
+	for _, nums := range numsToSum {
+		sums = append(sums, Sum(nums))
+	}
+
+	return sums
+}
+
+func SumAllTails(numsToSum ...[]int) []int {
+	var sums []int
+	for _, nums := range numsToSum {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(nums[1:]))
+		}
+	}
+
+	return sums
+}
--- tdd/arrays/sum_test.go
+++ tdd/arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -13,12 +14,47 @@ func TestSum(t *testing.T) {
 		assertTest(t, got, want, nums)
 	})
 
-	t.Run("for array [1,4,5,8,1] sum = 19", func(t *testing.T) {
-		nums := []int{1, 4, 5, 8, 1, 10}
+	t.Run("for array [1,8,1,10] sum = 20", func(t *testing.T) {
+		nums := []int{1, 8, 1, 10}
 
 		got := Sum(nums)
-		want := 29
+		want := 20
 		assertTest(t, got, want, nums)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	got := SumAll([]int{1, 2}, []int{0, 2})
+	want := []int{3, 2}
+	checkSums(t, got, want)
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("Testing non empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 4}, []int{3, 9})
+		want := []int{9, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("Testing for empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{})
+		want := []int{0}
+		checkSums(t, got, want)
 	})
 }
 
